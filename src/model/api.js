96ee334d5a68e965c2abbebce052cbf455c282df
邵励治 <shaolizhi1234@126.com>
2023-02-23 00:46:43 +0800

    finish api getEventList

--- src/model/api.js
+++ src/model/api.js
@@ -1,12 +1,22 @@
 import AV from "leancloud-storage";
 
 export default {
-  test: async () => {
+  /**
+   * 获取 Event 事件列表
+   * @param {*} prd_link
+   * @returns
+   */
+  getEventList: async prd_link => {
     try {
-      const eventList = await new AV.Query("event").find();
-      console.log("test successful", eventList);
-      return eventList;
+      const eventList = await new AV.Query("event")
+        .equalTo("prd_link", prd_link)
+        .find();
+
+      const result = eventList.map(event => event.toFullJSON());
+      console.log("getEventList successful", result);
+      return result;
     } catch (error) {
+      console.log("getEventList failed", error);
       throw error;
     }
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,12 @@
 import TheWelcome from "../components/TheWelcome.vue";
 import Api from "@/model/api.js";
 
-Api.test();
+// 要查询的 PRD 文档，对应的数据
+const prd_link =
+  "https://st94nif1cq.feishu.cn/wiki/wikcnT2mkPqNbbRklhri8rszPMg";
+
+// 获取 EventList
+const eventList = await Api.getEventList(prd_link);
 </script>
 
 <template>
