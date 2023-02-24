cc36796f6b107ca1cd58cc9220fc582c201ed483
邵励治 <shaolizhi1234@126.com>
2023-02-24 15:09:46 +0800

    完成了提交的工作

--- src/views/EventItem.vue
+++ src/views/EventItem.vue
@@ -3,7 +3,7 @@
     <!-- event-title -->
     <div class="event-title-container">
       <div class="event-title">{{ event.name }}</div>
-      <el-button>Submit</el-button>
+      <el-button @click="click_submit(event)">Submit</el-button>
     </div>
 
     <!-- event-testcase-list -->
@@ -19,10 +19,66 @@
 
 <script setup>
 import TestCaseItem from "@/views/TestCaseItem.vue";
+import Store from "@/store";
+import { inject } from "vue";
 
 defineProps({
   event: Object
 });
+
+// 要查询的 session_id
+const session_id = inject(Store.session_id);
+
+// 要查询的 mid
+const mid = inject(Store.mid);
+
+// 要查询的 uid
+const uid = inject(Store.uid);
+
+/**
+ * 点击 Submit 按钮
+ */
+const click_submit = event => {
+  const submitResult = {
+    event: {
+      objectId: event.objectId,
+      name: event.name,
+      description: event.description
+    },
+    testCaseList: [],
+    limitField: {
+      mid: mid.value,
+      uid: uid.value,
+      session_id: session_id.value
+    }
+  };
+
+  for (let i = 0; i < event.testCaseList.length; i++) {
+    const testCase = event.testCaseList[i];
+    if (testCase.is_tested) {
+      const submitTestCase = [];
+      testCase.finite_field_value_list.forEach(finite_field_value => {
+        submitTestCase.push({
+          field: {
+            objectId: finite_field_value.finite_field.objectId,
+            name: finite_field_value.finite_field.name,
+            description: finite_field_value.finite_field.description,
+            is_finite: finite_field_value.finite_field.is_finite,
+            extra: finite_field_value.finite_field.extra
+          },
+          value: {
+            objectId: finite_field_value.objectId,
+            name: finite_field_value.name,
+            description: finite_field_value.description
+          }
+        });
+      });
+      submitResult.testCaseList.push(submitTestCase);
+    }
+  }
+
+  console.log("submit", JSON.stringify(submitResult));
+};
 </script>
 
 <style lang="scss" scoped>
